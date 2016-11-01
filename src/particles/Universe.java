09551f0816e960beec0bf917357fa018c21ae2f6
Coleman Smith <csmitt23@gmail.com>
2016-10-31 22:39:32 -0400

    Started Implementing Universe
    Still have to add graphics to the class members

--- src/particles/Universe.java
+++ src/particles/Universe.java
@@ -3,9 +3,32 @@
  */
 
 /** Universe class meant to be extended to create a Universe that acts on the given physics */
+// ^^^possibly not meant to be extended???
 
 class Universe {
-  public Universe() {
-    
+  Force[] forces;
+  Particle[] particles;
+
+  public Universe(Force[] forces; Particle[] particles) {
+    this.forces = forces;
+    this.particles = particles;
+  }
+
+  public void run() {
+    while(True) {
+      step();
+    }
+  }
+  private void step() {
+    for(int i = 0; i < forces.length; i++) {
+      forces[i].calculateForce(particles);
+      for(int j = 0; j < particles.length; j++) {
+        forces[i].applyForce(particles[j]);
+      }
+    }
+    for(int i = 0; i < particles.length; i++) {
+      particles[i].update();
+    }
+
   }
 }
